bba736d4de5461ff8377dc453399c5558580f9ca
Peter Wiringa <wiringa@umn.edu>
2019-09-03 14:20:04 -0500

    conditional discount display for events and establishments

--- assets/js/app.js
+++ assets/js/app.js
@@ -1086,9 +1086,11 @@ var events = L.geoJson(null, {
       var content = "<table class='table table-striped table-bordered table-condensed'>" +
       "<tr><th scope='row'>Event Information</th><td>" + feature.properties.DETAILS + " (" + feature.properties.EVENTHOURS + ")</td></tr>" +
       "<tr><th scope='row'>Address</th><td>" + feature.properties.ADDRESS + "</td></tr>" +
-      "<tr><th scope='row'>Full Hours</th><td>" + feature.properties.HOURS + "</td></tr>" +
-      "<tr><th scope='row'>Discount</th><td>" + feature.properties.DISCOUNT_NOTES + "</td></tr>" +
-      "<tr><th scope='row'>Website</th><td><a class='url-break' href='" + feature.properties.URL + "' target='_blank'>" + feature.properties.URL + " (new window) <i class='fa fa-external-link-square'></i></a></td></tr><table>";
+      "<tr><th scope='row'>Full Hours</th><td>" + feature.properties.HOURS + "</td></tr>";
+      if ("DISCOUNT_NOTES" in feature.properties && feature.properties.DISCOUNT_NOTES !== "") {
+        content += "<tr><th scope='row'>Discount</th><td>" + feature.properties.DISCOUNT_NOTES + "</td></tr>";
+      }
+      content += "<tr><th scope='row'>Website</th><td><a class='url-break' href='" + feature.properties.URL + "' target='_blank'>" + feature.properties.URL + " (new window) <i class='fa fa-external-link-square'></i></a></td></tr><table>";
       layer.on({
         click: function (e) {
 	      $("#feature-title").html(feature.properties.NAME);
@@ -1180,8 +1182,12 @@ var establishments = L.geoJson(null, {
       var content = "<table class='table table-striped table-bordered table-condensed'>" +
       "<tr><th scope='row'>Address</th><td>" + feature.properties.ADDRESS + "</td></tr>" +
       "<tr><th scope='row'>Hours</th><td>" + feature.properties.HOURS + "</td></tr>" +
-      "<tr><th scope='row'>Website</th><td><a class='url-break' href='" + feature.properties.URL + "' target='_blank'>" + feature.properties.URL + " (new window) <i class='fa fa-external-link-square'></i></a></td></tr>" +
-      "<tr><th scope='row'>Discount</th><td>" + feature.properties.DISCOUNT_NOTES + "</td></tr>" + "<table>";
+      "<tr><th scope='row'>Website</th><td><a class='url-break' href='" + feature.properties.URL + "' target='_blank'>" + feature.properties.URL + " (new window) <i class='fa fa-external-link-square'></i></a></td></tr>";
+      if ("DISCOUNT_NOTES" in feature.properties && feature.properties.DISCOUNT_NOTES !== "") {
+        content += "<tr><th scope='row'>Discount</th><td>" + feature.properties.DISCOUNT_NOTES + "</td></tr>";
+      }
+
+      content += "</table>";
       layer.on({
         click: function (e) {
           $("#feature-title").html(feature.properties.NAME);
